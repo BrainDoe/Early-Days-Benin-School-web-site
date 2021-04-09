676e3d8b552a5e122cd50d9e78b6801fa6c4f00a
Donatus Okwe <braindona@gmail.com>
2021-04-09 10:02:59 -0700

    The commit for Eardays School Benin

---
+++ index.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from technext.github.io/unica/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 01 Mar 2021 15:38:20 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+	<title>EARLYDAYS MONTESORRI INTERNATIONAL SCHOOL</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Unica University Template">
+	<meta name="keywords" content="event, unica, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/themify-icons.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- header section -->
+	<header class="header-section">
+		<div class="container">
+			<!-- logo -->
+			<a href="index.php" class="site-logo"><img src="images/logo.png" alt=""></a>
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-info">
+				<div class="hf-item">
+					<i class="fa fa-clock-o"></i>
+					<p><span>Working time:</span>Monday - Friday: 08 AM - 06 PM</p>
+				</div>
+				<div class="hf-item">
+					<i class="fa fa-map-marker"></i>
+					<p><span>Find us:</span> <a href="mailto:info@earlydaysbenin.com.ng">info@earlydaysbenin.com.ng</a></p>
+					<p style="margin-left: 10px;"><span>Phone</span>0809 239 6633 </p>
+				</div> 				
+			</div>
+	</header>
+	<!-- header section end-->
+
+
+	<!-- Header section  -->
+	<nav class="nav-section">
+		<div class="container">
+			<div class="nav-right">
+				<a href="#"><i class="fa fa-search"></i></a>
+				<a href="#"><i class="fa fa-shopping-cart"></i></a>
+			</div>
+			<ul class="main-menu">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="#">Event</a></li>
+				<li><a href="course.php">COURSES</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- Header section end -->
+
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg" data-setbg="images/1.jpg">
+				<div class="hs-text">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="hs-subtitle" style="color: rgb(208, 208, 216);">SOARING HIGHER</div>
+								<h2 class="hs-title">Redirecting and Soaring Kids Enthusiasm For Learning.</h2>
+								<div class="site-btn">GET STARTED</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hs-item set-bg" data-setbg="images/3.jpg">
+				<div class="hs-text">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="hs-subtitle" style="color: rgb(208, 208, 216);">DEDICATED</div>
+								<h2 class="hs-title">DEDICATED AND QUALITY-RICH TEACHING</h2>
+								<div class="site-btn">KNOW MORE</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hs-item set-bg" data-setbg="images/4.jpg">
+				<div class="hs-text">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="hs-subtitle" style="color: rgb(208, 208, 216);">MAXIMUM STANDARD</div>
+								<h2 class="hs-title">SETTING THE BAR FOR EDUCATIONAL STANDARDS</h2>
+								<div class="site-btn">KNOW MORE</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+
+	<!-- Services section -->
+	<section class="service-section spad">
+		<div class="container services">
+			<div class="section-title text-center">
+				<h3>EARLY DAYS MONTESSORI SCHOOL BENIN</h3>
+				<p>The perfect bedrock for your child\s education</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/services-icons/4.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4>Fully Qualified Teachers</h4>
+						<p>Our experienced, compassionate teachers are ever ready to take through </p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/services-icons/5.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4>Perfect Learning Environment</h4>
+						<p>Our condusive classrooms and spacious environment is the best for your child.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/services-icons/6.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4>Optimized Lessons</h4>
+						<p>The best teaching method is what your child needs to understand 'difficult' concepts. We make learning fun for our students</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Services section end -->
+
+	
+	<!-- Enroll section -->
+	<section class="enroll-section spad set-bg" data-setbg="img/enroll-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<div class="section-title text-white">
+						<h3>ENROLLMENT</h3>
+						<p>Get started with us to explore the exciting</p>
+					</div>
+					<div class="enroll-list text-white">
+						<div class="enroll-list-item">
+							<span>1</span>
+							<h5>Contact</h5>
+							<p>5, Ugbomanta Street, Off Iziegbe Street, Off Aikhionbare,G.R.A
+								23401 Benin-City, Nigeria.</p>
+								<p><i class="fa fa-phone"></i> (+234) 809 239 6633</p>
+								<p><i class="fa fa-envelope"></i> info@earlydaysbenin.com.ng</p>
+						</div>
+						<div class="enroll-list-item">
+							<span>2</span>
+							<h5>Consulting</h5>
+							<p>We are open for consultation<p>
+								<p><i class="fa fa-phone"></i> (+234) 809 239 6633</p>
+								<p><i class="fa fa-envelope"></i> info@earlydaysbenin.com.ng</p>
+						</div>
+						<div class="enroll-list-item">
+							<span>3</span>
+							<h5>Register</h5>
+							<p>Don't wait, come and registered your child now!</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 offset-lg-1 p-lg-0 p-4">
+					<img src="images/94.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Enroll section end -->
+
+
+	<!-- Courses section -->
+	<section class="courses-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>OUR TEACHING TECHNIQUES ARE THE BEST</h3>
+				<p>Give Your Child the Perfect Foundation</p>
+			</div>
+			<div class="row">
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb">
+						<img src="images/9932.jpg" alt="">
+						<div class="course-cat">
+							<span>VOCATIONAL SKILLS</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<h4>Adequate training and guidance on relevant in-demand technological skills</h4>
+					</div>
+				</div>
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb">
+						<img src="images/93.jpg" alt="">
+						<div class="course-cat">
+							<span>COMPUTER ORIENTED</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<h4>Personalized attention on your child </h4>
+					</div>
+				</div>
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb">
+						<img src="images/9943.jpg" alt="">
+						<div class="course-cat">
+							<span>EXCURSIONS</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<h4>We provide our students the opportunity to explore and prepare for future tasks </h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Courses section end-->
+
+
+	<!-- Event section -->
+	<section class="event-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>OUR EVENTS</h3>
+				<p>We engage our students with students with series of fascinating and educative events</p>
+			</div>
+			<div class="row">
+				<div class="col-md-6 event-item">
+					<div class="event-thumb">
+						<img src="images/95.jpg" alt="">
+					</div>
+					<div class="event-info">
+						<h4>Wholesome social activities to boost their social life</h4>
+					</div>
+				</div>
+				<div class="col-md-6 event-item">
+					<div class="event-thumb">
+						<img src="images/96.jpg" alt="">
+					</div>
+					<div class="event-info">
+						<h4>Our students learn from real-life examples.</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Event section end -->
+
+
+	<!-- Gallery section -->
+	<div class="gallery-section">
+		<div class="section-title text-center">
+			<h3>OUR GALLERY</h3>
+			<p>Take a tour of highlights of some activities in our school</p>
+		</div>
+		<div class="gallery">
+			<div class="grid-sizer"></div>
+			<div class="gallery-item gi-big set-bg" data-setbg="images/2.jpg">
+				<a class="img-popup" href="images/2.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="images/5.jpg">
+				<a class="img-popup" href="images/5.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="images/6.jpg">
+				<a class="img-popup" href="images/6.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-long set-bg" data-setbg="images/7.jpg">
+				<a class="img-popup" href="images/7.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-big set-bg" data-setbg="images/9.jpg">
+				<a class="img-popup" href="images/9.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-long set-bg" data-setbg="images/61.jpg">
+				<a class="img-popup" href="images/61.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="images/9941.jpg">
+				<a class="img-popup" href="images/9941.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="images/9932.jpg">
+				<a class="img-popup" href="images/9932.jpg"><i class="ti-plus"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- Gallery section -->
+
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container footer-top">
+			<div class="row">
+				<!-- widget -->
+				<div class="col-sm-6 col-lg-3 footer-widget">
+					<div class="about-widget">
+						<img src="images/logo.png" alt="">
+					
+						<div class="social pt-1">
+							<a href="#"><i class="fa fa-twitter-square"></i></a>
+							<a href="#"><i class="fa fa-facebook-square"></i></a>
+							<a href="#"><i class="fa fa-google-plus-square"></i></a>
+							<a href="#"><i class="fa fa-linkedin-square"></i></a>
+							<a href="#"><i class="fa fa-rss-square"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- widget -->
+				<div class="col-sm-6 col-lg-3 footer-widget">
+					<h6 class="fw-title">USEFUL LINK</h6>
+					<div class="dobule-link">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">Events</a></li>
+						</ul>
+						<ul>
+							<li><a href="#">Policy</a></li>
+							<li><a href="#">Term</a></li>
+							<li><a href="#">Help</a></li>
+							<li><a href="#">FAQs</a></li>
+							<li><a href="#">Site map</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- widget -->
+				
+
+				<!-- widget -->
+				<div class="col-sm-6 col-lg-3 footer-widget">
+					<h6 class="fw-title">CONTACT</h6>
+					<ul class="contact">
+						<li><p><i class="fa fa-map-marker"></i> 5, Ugbomanta Street, Off Iziegbe Street, Off Aikhionbare, G.R.A	23401 Benin-City, Nigeria</p></li>
+						<li><p><i class="fa fa-phone"></i> (+234) 809 239 6633</p></li>
+						<li><p><i class="fa fa-envelope"></i> info@earlydaysbenin.com.ng</p></li>
+						<li><p><i class="fa fa-clock-o"></i> Monday - Friday, 08:00AM - 06:00 PM</p></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- copyright -->
+		<div class="copyright">
+			<div class="container">
+				<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | crafted by <a href="https://colorlib.com/" target="_blank">G-heli Technology Solutions</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+			</div>		
+		</div>
+	</footer>
+	<!-- Footer section end-->
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.countdown.js"></script>
+	<script src="js/masonry.pkgd.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+	
+</body>
+
+<!-- Mirrored from technext.github.io/unica/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 01 Mar 2021 15:38:45 GMT -->
+</html>
